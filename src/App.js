7656159b59e064e4fed0d4b9a002379c14bf2f87
AmirrHosein <amirhoseinrajabi07@gmail.com>
2022-07-17 02:18:33 +0430

    cleanup.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
+import Join from "./components/Join/Join";
+import Plans from "./components/Plans/Plans";
 import Programs from "./components/programs/Programs";
 import Reasons from "./components/Reasons/Reasons";
+import Testimonials from "./components/Testimonials/Testimonials";
 export default function App() {
   return (
     <div className="App">
       <Hero />
       <Programs />
       <Reasons />
+      <Plans />
+      <Testimonials />
+      <Join />
+      <Footer />
     </div>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,6 +9,7 @@ import calories from "../../assets/calories.png";
 export default function Hero() {
   return (
     <div className="hero">
+      <div className="blur hero-blur"></div>
       <div className="left-h">
         <Header />
         {/* the best ad */}
