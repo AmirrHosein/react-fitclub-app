1abe0e293264217163f78ba00748bc9f9ab3dba3
AmirrHosein <amirhoseinrajabi07@gmail.com>
2022-07-16 17:35:15 +0430

    add Programs section.

---
+++ src/components/programs/Programs.jsx
@@ -0,0 +1,30 @@
+import "./Programs.css";
+import { programsData } from "../../data/programsData";
+import RightArrow from "../../assets/rightArrow.png";
+
+export default function Programs() {
+  return (
+    <div className="Programs" id="programs">
+      {/* header */}
+      <div className="programs-header">
+        <span className="stroke-text">Explor our</span>
+        <span>Programs</span>
+        <span className="stroke-text">to shape you</span>
+      </div>
+
+      <div className="programs-categories">
+        {programsData.map((program) => (
+          <div className="category">
+            {program.image}
+            <span>{program.heading}</span>
+            <span>{program.details}</span>
+            <div className="join-now">
+              <span>Join Now</span>
+              <img src={RightArrow} alt="RightArrow" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
