84bed165b62f12222eb5c16a72370ae22bd17d49
AmirrHosein <amirhoseinrajabi07@gmail.com>
2022-07-16 04:40:35 +0430

    add Hero section.

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,80 @@
+import Header from "../Header/Header";
+import "./Hero.css";
+
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import calories from "../../assets/calories.png";
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <div className="left-h">
+        <Header />
+        {/* the best ad */}
+        <div className="the-best-ad">
+          <div></div>
+          <span>the best fitness club in the town</span>
+        </div>
+
+        {/* Hero Heading */}
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">shape</span>
+            <span>your</span>
+          </div>
+          <div>
+            <span>Ideal body</span>
+          </div>
+          <div>
+            <span>
+              In here we will help you to shape and build your ideal body and
+              live up your life to fullest
+            </span>
+          </div>
+        </div>
+        {/* figures */}
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>expert coachs</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness Programs</span>
+          </div>
+        </div>
+        {/* hero buttons */}
+        <div className="hero-buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
+        </div>
+      </div>
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+
+        <div className="heart-rate">
+          <img src={Heart} alt="heart" />
+          <span>Heart Rate</span>
+          <span>116 ppm</span>
+        </div>
+
+        {/* hero images */}
+        <img className="hero-image" src={hero_image} alt="" />
+        <img className="hero_image_back" src={hero_image_back} alt="" />
+        {/* calories */}
+        <div className="calories">
+          <img src={calories} alt="calories" />
+          <div>
+            <span>Calories Burned</span>
+            <span>220 kcal</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
